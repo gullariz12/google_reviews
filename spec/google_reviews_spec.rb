6578d33d613e1be06968a0166dd8c038d8cfe8e7
gullariz12 <gullariz82@gmail.com>
2023-07-09 22:04:39 +0500

    Rubocop fixes

--- spec/google_reviews_spec.rb
+++ spec/google_reviews_spec.rb
@@ -2,10 +2,10 @@
 
 RSpec.describe GoogleReviews do
   it "has a version number" do
-    #expect(GoogleReviews::VERSION).not_to be nil
+    # expect(GoogleReviews::VERSION).not_to be nil
   end
 
   it "does something useful" do
-    #expect(false).to eq(true)
+    # expect(false).to eq(true)
   end
 end
