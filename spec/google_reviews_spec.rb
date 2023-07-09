daefa2d4d3969cd84d11c73ef5254ff3a7ffd093
gullariz12 <gullariz82@gmail.com>
2023-07-09 12:23:39 +0500

    Initial commit

---
+++ spec/google_reviews_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe GoogleReviews do
+  it "has a version number" do
+    expect(GoogleReviews::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
